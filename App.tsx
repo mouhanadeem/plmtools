3aa4f5abb0e9b776ea9a9518abfcaf3eb997caa7
Nadeem Muhammad <31960661+mouhanadeem@users.noreply.github.com>
2021-06-17 15:02:53 +0200

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Textarea } from "@lego/klik-ui";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Textarea placeholder="Placeholder text" />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
